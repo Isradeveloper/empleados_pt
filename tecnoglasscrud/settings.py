e752065d393547fe1a259f64cbe635891288af94
Israel Trujillo Dominguez <ingisraeltrujillo@gmail.com>
2023-01-21 22:20:24 -0500

    fix?

--- tecnoglasscrud/settings.py
+++ tecnoglasscrud/settings.py
@@ -149,9 +149,9 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
